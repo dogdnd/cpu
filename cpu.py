c147e8d6547b44b3844707b62bf7890babeb22e3
dogdnd <dogdnd@gmail.com>
2022-06-08 11:54:56 +0900

    main

--- cpu.py
+++ cpu.py
@@ -25,6 +25,8 @@ st.image('https://s19538.pcdn.co/wp-content/uploads/2021/09/semiconductors.jpg')
 def main() :
     df = pd.read_csv('data/CPU_r23_v2.csv')
 
+    
+
 
 
 
@@ -44,10 +46,13 @@ def main() :
         fig = px.bar(df, x=df['cores'], y=df['threads'], color=df['manufacturer'])
         st.plotly_chart(fig)
 
+        fig = px.bar(df, x=df['baseClock'], y=df['turboClock'], color=df['manufacturer'],)
+        st.plotly_chart(fig)
+
 
         df1 = df['manufacturer'].value_counts()
         df1 = pd.DataFrame(df1)
-        fig1 = px.pie(df1, names= df1.index, values=df1['manufacturer'], title= 'core')
+        fig1 = px.pie(df1, names= df1.index, values=df1['manufacturer'])
         st.plotly_chart(fig1)
 
 
@@ -58,19 +63,41 @@ def main() :
     
     
     st.info('클럭별 cpu 정보')
-    baseClock = st.slider('baseClock', 0.0, 4.3, 0.1 , 0.1)
-    turboClock = st.slider('turboClock', 0.0, 6.0 , 0.1, 0.1)
-    df1 = df.loc[  df['baseClock'] >= baseClock ,   ['cpuName','baseClock','turboClock'] ] 
-    df2 = df1.loc[  df['turboClock'] >= turboClock   ,   ]
+    baseClock_choice = st.slider('baseClock', 0.0, 4.3, 0.1 , 0.1)
+    turboClock_choice = st.slider('turboClock', 0.0, 6.0 , 0.1, 0.1)
+    df1 = df.loc[  df['baseClock'] >= baseClock_choice ,   ['cpuName','baseClock','turboClock'] ] 
+    df2 = df1.loc[  df['turboClock'] >= turboClock_choice   ,   ]
     st.dataframe(df2)
+
+
+    st.info('CPU 요소별 count treemap')
+    treemap_manu = ['baseClock','turboClock','cores','threads']
+    treemap_choice = st.selectbox('선택',treemap_manu)
+    
+    if treemap_choice == treemap_manu[0] :
+        ax= px.treemap(df,path=["baseClock"])
+        st.plotly_chart(ax)
+        
+    elif treemap_choice == treemap_manu[1] :
+        ax= px.treemap(df,path=["turboClock"])
+        st.plotly_chart(ax)
+
+    elif treemap_choice == treemap_manu[2] :
+        ax= px.treemap(df,path=["cores"])
+        st.plotly_chart(ax)
+
+    elif treemap_choice == treemap_manu[3] :
+        ax= px.treemap(df,path=["threads"])
+        st.plotly_chart(ax)
+    
     
     
     st.info('CPU 요소별 상관관계 분석그래프')
-    choice2 = st.multiselect('요소 선택',df.columns)
+    pair_choice = st.multiselect('요소 선택',df.columns)
 
-    if len(choice2) != 0 :
-        st.dataframe(df[choice2].corr())
-        fig1 = sns.pairplot(data = df, vars = choice2 )
+    if len(pair_choice) != 0 :
+        st.dataframe(df[pair_choice].corr())
+        fig1 = sns.pairplot(data = df, vars = pair_choice )
         st.pyplot(fig1)
 
 
@@ -109,10 +136,10 @@ def main() :
 
 
     st.info('cpu 요소별 점수예측 프로그램')
-    core_input = st.slider('core')
-    thread_input = st.slider('thread',max_value=200)
-    base_input = st.slider('base', 0.1, 5.0, 0.1, 0.1)
-    turbo_input = st.slider('turbo', 0.1,5.5, 0.1, 0.1)
+    core_input = st.slider('core',max_value=64)
+    thread_input = st.slider('thread',max_value=128)
+    base_input = st.slider('base', 0.1, 4.0, 0.1, 0.1)
+    turbo_input = st.slider('turbo', 0.1, 5.4, 0.1, 0.1)
 
     cpu_info = np.array([core_input,thread_input,base_input,turbo_input])
     cpu_info = cpu_info.reshape(1,4)
