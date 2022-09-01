a3d078b4d3c8ebe87273ac45667992e322052ecb
dogdnd <dogdnd@gmail.com>
2022-09-01 17:42:17 +0900

    main

--- cpu.py
+++ cpu.py
@@ -18,7 +18,7 @@ row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
 with row3_1:
     st.markdown("made by dogdnd")
     st.markdown("used library : pandas, numpy, streamlit, matplotlib.pyplot, seaborn, joblib")
-    st.markdown("[GitHub Repository](https://github.com/dogdnd/streamlit_1)")
+    st.markdown("[GitHub Repository](https://github.com/dogdnd/cpu)")
 
 st.image('https://s19538.pcdn.co/wp-content/uploads/2021/09/semiconductors.jpg')
 
@@ -31,7 +31,7 @@ def main() :
 
 
 
-    st.info('원하는 CPU 정보를 선택하시오')
+    st.info('컬럼별 CPU 정보 데이터프레임')
     column_list = st.multiselect('선택', df.columns)
     if len(column_list) != 0 :
         st.dataframe(df[column_list])
@@ -42,95 +42,116 @@ def main() :
     st.dataframe(df.loc[df['manufacturer'] == choice , ])
 
     
-    if st.button('CPU by manufacturer') :
+    if st.checkbox('제조사별 CPU 데이터 그래프') :
 
-        fig = px.bar(df, x=df['cores'], y=df['threads'], color=df['manufacturer'])
+        fig = px.bar(df, x=df['cores'],title= '제조사별 코어스레드 그래프' ,y=df['threads'], color=df['manufacturer'])
         st.plotly_chart(fig)
 
-        fig = px.bar(df, x=df['baseClock'], y=df['turboClock'], color=df['manufacturer'],)
+        fig = px.bar(df, x=df['baseClock'],title= '제조사별 클럭 그래프', y=df['turboClock'], color=df['manufacturer'],)
         st.plotly_chart(fig)
 
 
         df1 = df['manufacturer'].value_counts()
         df1 = pd.DataFrame(df1)
-        fig1 = px.pie(df1, names= df1.index, values=df1['manufacturer'])
+        fig1 = px.pie(df1, names= df1.index,title='제조사별 cpu 보유 차트',values=df1['manufacturer'])
         st.plotly_chart(fig1)
 
-
-    
-    
     
 
-    
-    
-    st.info('클럭별 cpu 정보')
-    baseClock_choice = st.slider('baseClock', 0.0, 4.3, 0.1 , 0.1)
-    turboClock_choice = st.slider('turboClock', 0.0, 6.0 , 0.1, 0.1)
-    df1 = df.loc[  df['baseClock'] >= baseClock_choice ,   ['cpuName','baseClock','turboClock'] ] 
-    df2 = df1.loc[  df['turboClock'] >= turboClock_choice   ,   ]
-    st.dataframe(df2)
-
-
-    st.info('CPU 요소별 count treemap')
-    treemap_manu = ['baseClock','turboClock','cores','threads']
-    treemap_choice = st.selectbox('선택',treemap_manu)
-    
-    if treemap_choice == treemap_manu[0] :
-        ax= px.treemap(df,path=["baseClock"])
-        st.plotly_chart(ax)
         
-    elif treemap_choice == treemap_manu[1] :
-        ax= px.treemap(df,path=["turboClock"])
-        st.plotly_chart(ax)
 
-    elif treemap_choice == treemap_manu[2] :
-        ax= px.treemap(df,path=["cores"])
-        st.plotly_chart(ax)
 
-    elif treemap_choice == treemap_manu[3] :
-        ax= px.treemap(df,path=["threads"])
-        st.plotly_chart(ax)
     
     
     
-    st.info('CPU 요소별 상관관계 분석그래프')
-    pair_choice = st.multiselect('요소 선택',df.columns)
+
+    
+    
+    st.info('Clock 구간별 CPU 정보')
+    baseClock_choice = st.slider('baseClock', 1.1, 4.0, 1.1 , 0.1)
+    turboClock_choice = st.slider('turboClock', 3.2, 5.0 , 3.2, 0.1)
+    df1 = df.loc[  df['baseClock'] <= baseClock_choice ,   ['cpuName','baseClock','turboClock','singleScore','multiScore'] ] 
+    df2 = df1.loc[  df['turboClock'] <= turboClock_choice   ,   ]
+    
+    if st.checkbox("해당 조건에 맞는 CPU 보기") :
+        st.dataframe(df2.sort_values(['baseClock','singleScore'],ascending = True))
+
+        st.info('제조사별 Flagship CPU')
+        top_cpu_by_manufacturer = df.groupby('manufacturer')[['manufacturer','singleScore','multiScore','cores','threads','baseClock','turboClock']].max()
+        st.dataframe(top_cpu_by_manufacturer)
+
+
+
+    st.info('CPU 요소별 비율 차트')
+    treemap_menu = ['baseClock','turboClock','cores','threads']
+    treemap_choice = st.selectbox('선택',treemap_menu)
+
+    if st.checkbox('트리맵 차트 보기') :
+
+    
+        if treemap_choice == treemap_menu[0] :
+            ax= px.treemap(df,path=["baseClock"])
+            st.plotly_chart(ax)
+            
+        elif treemap_choice == treemap_menu[1] :
+            ax= px.treemap(df,path=["turboClock"])
+            st.plotly_chart(ax)
+
+        elif treemap_choice == treemap_menu[2] :
+            ax= px.treemap(df,path=["cores"])
+            st.plotly_chart(ax)
+
+        elif treemap_choice == treemap_menu[3] :
+            ax= px.treemap(df,path=["threads"])
+            st.plotly_chart(ax)
+    
+    
+    
+    st.info('CPU 구성 요소에 따른 상관관계 분석그래프')
+
+
+    column_list = df.columns[2:-1]
+    pair_choice = st.multiselect('상관관계 선택',column_list)
 
     if len(pair_choice) != 0 :
         st.dataframe(df[pair_choice].corr())
         
-        if st.button('상관관계 그래프 보기') :
+        if st.checkbox('상관관계 그래프 보기') :
             fig1 = sns.pairplot(data = df, vars = pair_choice )
             st.pyplot(fig1)
 
 
     
-    st.info('ALTAIR 분포도')
+    st.info('ALTAIR 분포도 그래프')
     
     list2 = ['cores & threads','clocks']
     choice2 = st.selectbox('선택',list2)
-    if choice2 == list2[0] :
-        a = alt.Chart(df).mark_circle().encode(
-        x='singleScore', 
-        y='multiScore', 
-        size='cores', 
-        color='threads', 
-        tooltip=['singleScore', 'multiScore','cores','threads']
-        )
-        st.altair_chart(a, use_container_width=True)
-    
-   
-    elif choice2 == list2[1] :
-        a = alt.Chart(df).mark_circle().encode(
-        x='singleScore', 
-        y='multiScore', 
-        size='baseClock', 
-        color='turboClock', 
-        tooltip=['singleScore', 'multiScore','baseClock','turboClock']
-        )
-        
-        st.altair_chart(a, use_container_width=True)
-    
+
+    if st.checkbox('그래프 보기',) :
+
+
+        if choice2 == list2[0] :
+            a = alt.Chart(df).mark_circle().encode(
+            x='singleScore', 
+            y='multiScore', 
+            size='cores', 
+            color='threads', 
+            tooltip=['singleScore', 'multiScore','cores','threads']
+            )
+            st.altair_chart(a, use_container_width=True)
+
+
+        elif choice2 == list2[1] :
+            a = alt.Chart(df).mark_circle().encode(
+            x='singleScore', 
+            y='multiScore', 
+            size='baseClock', 
+            color='turboClock', 
+            tooltip=['singleScore', 'multiScore','baseClock','turboClock']
+            )
+            
+            st.altair_chart(a, use_container_width=True)
+
 
 
     
@@ -139,11 +160,11 @@ def main() :
     scaler_x = joblib.load('data/scaler_x.pkl')
 
 
-    st.info('cpu 요소별 점수예측 프로그램')
+    st.info('ML을 활용한 cpu 점수예측 프로그램')
     core_input = st.slider('core',max_value=64)
     thread_input = st.slider('thread',max_value=128)
-    base_input = st.slider('base', 0.1, 4.0, 0.1, 0.1)
-    turbo_input = st.slider('turbo', 0.1, 5.4, 0.1, 0.1)
+    base_input = st.slider('base', 1.1, 4.0, 1.1, 0.1)
+    turbo_input = st.slider('turbo', 3.2, 5.0, 3.2, 0.1)
 
     cpu_info = np.array([core_input,thread_input,base_input,turbo_input])
     cpu_info = cpu_info.reshape(1,4)
@@ -152,32 +173,14 @@ def main() :
     
     cpu_pred = cpu_pred.tolist()
 
-    if st.button('벤치마크 예측') :
 
+
+    # 익스펜더
+    with st.expander('벤치마크 점수 예측') :
         st.write('예측된 싱글코어 점수는' ,round(cpu_pred[0][0]), '입니다.')
         st.write('예측된 멀티코어 점수는' ,round(cpu_pred[0][1]), '입니다.')
 
 
-    
-     
-     
-    if st.button('TOP CPU by manufacturer') :
-        top_cpu_by_manufacturer = df.groupby('manufacturer')[['manufacturer','singleScore','multiScore','cores','threads','baseClock','turboClock']].max()
-        st.dataframe(top_cpu_by_manufacturer)
-
-
-        fig = px.bar( top_cpu_by_manufacturer , x= 'singleScore' , y='multiScore', color='manufacturer' )
-        st.plotly_chart(fig)
-
-
-
-
-        
-        
-    
-
-
-    
 
 
 
